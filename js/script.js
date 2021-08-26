23a525ae0eb3f2d7a8782d2c0b216ae114851923
Aneckon <83111616+Aneckon@users.noreply.github.com>
2021-08-26 08:29:28 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
       $('.header__menu').toggleClass('menu__open');
       $('.header__button').toggleClass('menu__open');
       $('.header__burder-img').toggleClass('remove');
+      $('body').toggleClass('fixed');
   });
 });
